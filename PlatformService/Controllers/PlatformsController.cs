9201dce6d17372b66c718899c1ce2b50b377bb11
willis <soeasy2850@hotmail.com>
2021-09-12 12:33:46 +0800

    基本API完成

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -3,10 +3,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
-    [Route("api/[Controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PlatformsController:ControllerBase
     {
@@ -23,6 +24,33 @@ namespace PlatformService.Controllers
         public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms(){
             var platformItem = _repository.GetAllPlatforms();
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var PlatformItem = _repository.GetPlatformById(id);
+            if (PlatformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(PlatformItem));
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CraetePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            // 呼叫現有的Action，該Action要宣告名稱
+            return CreatedAtRoute(nameof(GetPlatformById), new {id = platformModel.Id}, platformReadDto);;
+
         }
     }
 }
